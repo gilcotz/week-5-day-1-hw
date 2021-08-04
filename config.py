4487b80573c5f3425e3846c120859c50208fcd47
gilcotz <87039963+gilcotz@users.noreply.github.com>
2021-08-04 00:06:48 -0700

    This is hw-2

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+  SECRET_KEY= "You will never guess..."
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
